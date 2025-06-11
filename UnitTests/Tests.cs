e82fb585a06771c5cc472047bac94dff1b46b8c4
Anton <noten.b@hotmail.com>
2025-06-11 16:01:30 +0200

    🐋 Docker (#6)
    * 🐋 Add dockerfile
    
    * 🔧 Fix base output
    
    * 💡 Update gitignore

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -7,7 +7,7 @@ public class Tests {
     [Test]
     public void Serializer() {
         var rng = new Random();
-        for (int i = 0; i < 10 ; i++) {
+        for (int i = 0; i < 100 ; i++) {
             ushort id = (ushort)rng.Next(0, ushort.MaxValue);
             int x = rng.Next(0, 16);
             int y = rng.Next(0, 16);
@@ -17,6 +17,8 @@ public class Tests {
             Assert.AreEqual(id, result.id);
             Assert.AreEqual(x, result.x);
             Assert.AreEqual(y, result.y);
+            Assert.IsFalse(serialized.Contains('/'));
+            Assert.IsFalse(serialized.Contains('='));
         }
     }
 }
